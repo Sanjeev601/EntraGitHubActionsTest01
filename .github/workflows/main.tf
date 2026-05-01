c78e6b77c368335c6851d6a0aaa8fb4abdfec7fb
Sanjeev601 <30383706+Sanjeev601@users.noreply.github.com>
2026-05-01 12:48:55 +0530

    craeting app reg v1

---
+++ .github/workflows/main.tf
@@ -0,0 +1,10 @@
+provider "azuread" {}
+
+resource "azuread_application" "TestApp" {
+  display_name     = "TestApp"
+  sign_in_audience = "AzureADMyOrg"
+}
+
+resource "azuread_service_principal" "app_sp" {
+  application_id = azuread_application.TestApp.application_id
+}
